c233d88a57ba4923f5e19b0b5eef1c176196bc9e
name-of-okja <nameofokja@gmail.com>
2024-04-19 19:47:44 +0900

    Add Common DI

---
+++ src/CleanArchitecture.Application/ApplicationServcieRegister.cs
@@ -0,0 +1,20 @@
+﻿using CleanArchitecture.Application.Behaviors;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace CleanArchitecture.Application;
+public static class ApplicationServcieRegister
+{
+    public static IServiceCollection AddApplicationServcies(this IServiceCollection services)
+    {
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+        services.AddMediatR(config => config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehavior<,>));
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+        return services;
+    }
+}
