ef74a226a9014a11a44d2f9e4f3e8b15e70a07b7
name-of-okja <nameofokja@gmail.com>
2024-05-01 22:24:13 +0900

    Create Repository Layout

---
+++ src/CleanArchitecture.Data/Repositories/VideoRepository.cs
@@ -0,0 +1,23 @@
+﻿
+using CleanArchitecture.Application.Contracts.Persistence;
+using CleanArchitecture.Domain;
+using CleanArchitecture.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitecture.Infrastructure.Repositories;
+public class VideoRepository : RepositoryBase<Video>, IVideoRepository
+{
+    public VideoRepository(StreamerDbContext context) : base(context)
+    {
+        
+    }
+    public async Task<Video> GetVideoByName(string videoName)
+    {
+        return await _context.Videos.Where(o => o.Name == videoName).SingleOrDefaultAsync();
+    }
+
+    public async Task<IEnumerable<Video>> GetVideosByUsername(string username)
+    {
+        return await _context.Videos.Where(v => v.CreatedBy == username).ToListAsync();
+    }
+}
